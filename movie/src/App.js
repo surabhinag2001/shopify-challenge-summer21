a100c73aaee68e76aae6fdb53457140d3250a1e3
Surabhi Nag <snag2001@students.cs.ubc.ca>
2021-01-17 15:40:09 +0530

    Realtime search results being displayed

--- movie/src/App.js
+++ movie/src/App.js
@@ -3,13 +3,21 @@ import './App.css';
 import axios from "axios";
 import 'semantic-ui-css/semantic.min.css';
 import { Search, Grid, Header, Segment } from 'semantic-ui-react'
+import { useState } from 'react';
 function App() {
-
+  const [searchResult, setSearchResult] = useState(null);
   const fetchData = async (searchQuery) => {
     const response = await axios.get(
       `http://www.omdbapi.com/?apikey=1a54ad07&s=${searchQuery}`
     );
     console.log(response.data);
+    if (response.data.Response === "True") {
+      setSearchResult(response.data);
+      console.log(response.data.Search);
+      console.log(searchResult);
+    }
+
+
   };
 
   return (
@@ -18,19 +26,37 @@ function App() {
         <Search
           open={false}
           onSearchChange={(e, data) => {
-            console.log(data);
+            //console.log(data);
             fetchData(data.value)
           }}
         />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <div>
+        {/* <div>
           <button className="fetch-button" onClick={fetchData}>
             Fetch Data
         </button>
           <br />
+        </div> */}
+
+
+        <div className="movies">
+          {searchResult &&
+            searchResult.Search.map((movie, index) => {
+              const title = movie.Title;
+              const year = movie.Year;
+
+              return (
+                <div className="movie" key={index}>
+                  <h3>Movie: {title} </h3>
+                  <h3>Year: {year}</h3>
+                </div>
+              );
+            })}
+
         </div>
+
 
         <a
           className="App-link"
@@ -41,7 +67,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div >
   );
 }
 
